d5e2874577482de99cf4e3b4cd4ca366e4987ce0
ATERMAio <info@aterma.io>
2024-08-14 22:46:25 +0300

    fixed error on vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { TrashIcon, CheckIcon } from "@heroicons/react/24/outline";
 import { useLocalStorageState } from "./useLocalStorageState";
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
